1f884d41a617c89becc89e5395cc382b6f1a7f3d
javiergraphico <javieralexander38@hotmail.com>
2023-11-10 16:17:32 -0400

    novetaynueveporciento

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import styles from "../styles/navbar.module.css";
 import Link from "next/link";
 import { RiMenu4Fill, RiCloseFill } from "react-icons/ri";
